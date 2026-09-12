b268dce68007d0c99fae10acdcb37852e2999ea0
Alireza <89458295+Alireza-Rajoli-Nowdeh@users.noreply.github.com>
2026-09-11 22:50:40 -0400

    Implement fast approach for finding error numbers
    Added a fast approach using sets to find duplicate and missing numbers.

--- Easy/Set-Mismach.py
+++ Easy/Set-Mismach.py
@@ -1,3 +1,5 @@
+#1)Slow approach
+
 # using 2 for loops for checking candidate number and list elements/ range (start, stop) how it works/ using checkers after inner loops
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
@@ -12,3 +14,16 @@ class Solution:
             if K ==0:
                 ans.append(i)
         return ans
+
+#2) Fast appraoch
+
+# Using "set ()" instead of scanning / Using "in" and "Not in" / Using "Sum()"
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        snums = set(nums)
+        for i in range(1, len(nums)+1):
+            if i not in snums:
+                missing = i
+        duplicate = sum(nums)-sum(snums)
+        return [duplicate, missing]
