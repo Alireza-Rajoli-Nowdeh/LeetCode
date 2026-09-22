1793039fdd3456bedce73c14ae5e17e0d2315618
Alireza <89458295+Alireza-Rajoli-Nowdeh@users.noreply.github.com>
2026-09-22 00:55:53 -0400

    Optimize dailyTemperatures function with stack approach
    Refactor dailyTemperatures to use a stack for efficiency.

--- Medium/Daily-tempratures.py
+++ Medium/Daily-tempratures.py
@@ -1,4 +1,15 @@
-#Fast version 
+#Fast version/ Top of stack is stack[-1]/ using while() for popping untill condition stasfied
+class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        ans=[0]*len(temperatures)
+        stack=[]
+        for i in range(len(temperatures)):
+            while len(stack) > 0 and temperatures[i]> temperatures[stack[-1]]:
+                popped = stack.pop()
+                ans[popped] = i - popped
+            stack.append(i)
+
+        return ans
 
 #Slow version 
 class Solution:
